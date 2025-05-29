8844a1fb83a5054467c7efd71cb7f5fab7c36261
George <georgios.orfanidis4@gmail.com>
2025-05-29 17:55:52 +0300

    Added basic api connection and interface by fetching some results. Added Search functionality.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
-
+import './index.css'
 import './App.css'
-import Card from './Card.jsx'
+import Card from './components/Card.jsx'
+import Search from './components/Search.jsx'
 import batmanJPG from './assets/batman.png'
 import infWarJPG from './assets/infWar.webp'
 import moonLightJPG from './assets/moonlight.jpg'
-import searchIcon from './assets/search.svg'
+import { useState, useEffect, use } from 'react'
 
+const API_BASE_URL= 'https://api.themoviedb.org/3';
+
+const API_KEY= import.meta.env.VITE_TMDB_API_KEY;
+
+const API_OPTIONS = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization: `Bearer ${API_KEY}`
+  }
+};
 const cardData = [
   {
     title: "Batman",
@@ -28,6 +40,44 @@ const cardStyles = [
 ];
 
 function App() {
+  const [searchTerm, setSearchTerm] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
+  const [movieList, setMovieList] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const fetchMovies = async () => {
+    setIsLoading(true);
+    setErrorMessage('');
+
+    try {
+      const endpoint = `${API_BASE_URL}/discover/movie?sort_buy=popularity.desc`;
+
+      const res = await fetch(endpoint, API_OPTIONS)
+
+      if(!res.ok){
+        throw new Error('Failed to fetch movies!');
+      }
+
+      const data = await res.json();
+
+      if(data.res === 'False'){
+        setErrorMessage(data.Error || 'Failed to fetch movies');
+        setMovieList([]);
+        return;
+      }
+
+      setMovieList(data.results || []);
+    } catch (error) {
+      console.error(`Error fetching movies: ${error}`);
+      setErrorMessage('Error fetching movies. Please try again later');
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
+  useEffect(() => {
+    fetchMovies();
+  }, [])
 
   return (
     <main>
@@ -48,13 +98,26 @@ function App() {
             ))}
           </div>
 
-          <h1>Find Movies You'll Enjoy without a Hassle</h1>
+          <h1 className='text-white'>Find <span className='bg-gradient-to-r from-blue-50 to-blue-500 bg-clip-text text-transparent'>Movies</span> You'll Enjoy without a Hassle</h1>
+
+           <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm}/>
         </header>
 
-        <p className='searchBar'>
-          <button onClick={() => searchMovie()}><img src={searchIcon} alt="Search icon" /></button>
-          <input type="text" placeholder='Search through 300+ movies online'/>
-        </p>
+        <section className='allMovies'>
+          <h2>All Movies</h2>
+
+          {isLoading ? (
+            <p className='text-white'>Loading...</p>
+          ) : errorMessage ? (
+            <p className='text-red-500'>{errorMessage}</p>
+          ) : (
+            <ul>
+              {movieList.map((movie) => (
+                <p className='text-white'>{movie.title}</p>
+              ))}
+            </ul>
+          )}
+        </section>
       </div>
     </main>
   );
