1a0dc1961c8a1cf85cda0ba98fe9fe15a2a98a19
George <georgios.orfanidis4@gmail.com>
2025-07-01 13:30:40 +0300

    Made changes to api calling , added query.

--- src/services/api.js
+++ src/services/api.js
@@ -10,11 +10,19 @@ const API_OPTIONS = {
   }
 };
 
-export const fetchMovies = async () => {
-      const endpoint = `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
+export const fetchMovies = async (query = '') => {
+   console.log('fetchMovies called with query:', JSON.stringify(query));
+  let url = '';
+  
+     if (query.trim()) {
+    
+        const encodedQuery = encodeURIComponent(query);
+        url = `${API_BASE_URL}/search/movie?query=${encodedQuery}`;
+      } else {
+        url =  `${API_BASE_URL}/discover/movie?sort_by=popularity.desc&language=en-US&region=US`;
+      }
 
-      const res = await fetch(endpoint, API_OPTIONS)
-
+      const res = await fetch(url, API_OPTIONS);
       if(!res.ok){
         throw new Error('Failed to fetch movies!');
       }
